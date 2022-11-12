b1cd4e525ff1da6caeaedd09e78a3ade319f5b05
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2022-11-12 11:42:51 -0500

    CasoFecha V2.0. TomasCv0628

--- modelo/Fecha.java
+++ modelo/Fecha.java
@@ -11,6 +11,41 @@ public class Fecha
     private int mes;
     private int annio;
 
+    public Fecha(int d,int m , int a)
+    {
+        this.dia = d;
+        this.mes = m;
+        this.annio = a;
+    }
+    public Fecha()
+    {
+
+    }
+    // Constructor con parametro String dd/mm/aaaa
+    /* 
+    public Fecha(String s)
+    {
+        String[] fecha = s.split("/");
+        this.dia = Integer.parseInt(fecha[0]);
+        this.mes = Integer.parseInt(fecha[1]);
+        this.annio = Integer.parseInt(fecha[2]);
+    }
+    */
+    public Fecha(String s)
+    {
+        int pos1 = s.indexOf('/');
+        int pos2 = s.lastIndexOf('/');
+
+        String sDia = s.substring(0,pos1);
+        dia = Integer.parseInt(sDia);
+
+        String sMes = s.substring(pos1+1,pos2);
+        mes = Integer.parseInt(sMes);
+
+        String sAnio = s.substring(pos2+1);
+        annio = Integer.parseInt(sAnio);
+    }
+
     public int getDia()
     {
         return dia;
@@ -37,6 +72,35 @@ public class Fecha
     }
     public String toString()
     {
-        return dia + "/" + mes + "/" + annio;
+        return "Fecha: " + dia + "/" + mes + "/" + annio;
     }
+    public boolean equals(Object o)
+    {
+        Fecha otra = (Fecha)o;
+        return (dia == otra.dia) && (mes == otra.mes) && (annio == otra.annio);
+    }
+    public void addDias(int d)
+    {
+        int sum = fechaToDias() + d;
+        diasToFecha(sum);
+    }
+    private int fechaToDias()
+    {
+        return(((annio)*(12*30))+((mes*30)+dia));   
+    }
+    private void diasToFecha(int i)
+    {
+        int e = (i / 360);
+        int e2 = ((i-(e*360))/30);
+        int e3 = (i-((e * 360)+(e2*30)));
+        annio = e;
+        mes = e2;
+        dia = e3;
+        if((mes + e2) > 12)
+        {
+            annio = annio + 1;
+            mes = mes - 12;
+        }
+    }
+
 }
